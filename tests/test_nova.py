195131d5d9f974720598d814bc54dccd27d18980
jenkins <jenkins@bcpc>
2019-03-19 15:04:47 -0400

    Add nova tests

--- tests/test_nova.py
+++ tests/test_nova.py
@@ -14,3 +14,20 @@ def test_services_head(host, name):
     s = host.service(name)
     assert s.is_running
     assert s.is_enabled
+
+@pytest.mark.headnodes
+@pytest.mark.worknodes
+@pytest.mark.production
+def test_memcache_config(host):
+    nova_conf = host.file("/etc/nova/nova.conf")
+    assert nova_conf.contains("backend = oslo_cache.memcache_pool")
+    assert nova_conf.user == "root"
+    assert nova_conf.group == "nova"
+    assert nova_conf.mode == 0o640
+
+@pytest.mark.headnodes
+@pytest.mark.production
+def test_nova_version(host):
+    nova_pkg = host.package("nova-api")
+    assert nova_pkg.is_installed
+    assert nova_pkg.version == "2:18.0.3-0ubuntu2~cloud0"
